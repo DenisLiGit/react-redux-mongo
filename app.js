884f0ab5e4428e9a5726698e6685f9342d7064df
Den L <denisimusiii@gmail.com>
2021-02-15 20:05:51 +0300

    234

--- app.js
+++ app.js
@@ -8,11 +8,10 @@ const express = require('express')
 const config = require('config')
 const mongoose = require('mongoose')
 const cron = require("node-cron")
-const path = require("path")
 
 const app = express()
 
-const PORT = server.listen(process.env.PORT || 8080);
+const PORT = process.env.PORT || 5000;
 // const PORT = config.get('port') || 5000
 
 app.use(express.json({extended: true}))
