d039a43f81f41860aa1a80baec99a45a8072fb83
Den L <denisimusiii@gmail.com>
2021-02-15 19:30:36 +0300

    default port

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ const path = require("path")
 
 const app = express()
 
-const PORT = config.get('port') || 5000
+const PORT = process.env.PORT || 5000
+// const PORT = config.get('port') || 5000
 
 app.use(express.json({extended: true}))
 
