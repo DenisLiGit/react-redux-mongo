996b1fc1319303547be465d2f7a601eeb3d1ea1b
Den L <denisimusiii@gmail.com>
2021-02-15 19:56:11 +0300

    m,hkjfrts

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/statistic', require('./routes/statistic.routes'))
 
 if (process.env.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use('/', express.static(__dirname+'/client'+'/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(__dirname+ '/client'+ '/build'+ '/index.html')
     })
 }
 
